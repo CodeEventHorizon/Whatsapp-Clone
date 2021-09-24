d90723a7b7d0f8a8a3572c5a681f2d34975d897c
Nikoloz Muladze <nikoloz.9374@gmail.com>
2021-09-24 20:28:52 +0400

    Made a square, with flex 0.35 sidebar and flex 0.65 chat containers

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import Chat from './Chat';
+import Sidebar from './Sidebar';
 
 function App() {
   return (
     <div className="app">
-      <h1>Hello</h1>
+      <div className="app_body">
+        <Sidebar />
+        <Chat />
+      </div>
     </div>
   );
 }
---
+++ src/Chat.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Chat.css'
+
+function Chat() {
+    return (
+        <div className='chat'>
+            <h1>Hello im chat</h1>
+        </div>
+    )
+}
+
+export default Chat
