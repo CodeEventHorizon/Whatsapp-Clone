52aa4171403471cca7ffa06e1444a5a7869cdee8
Nikoloz Muladze <nikoloz.9374@gmail.com>
2021-09-24 19:23:59 +0400

    Added firebase config

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyApInUl5caiTLeeRR4ts9rjXpsip-SZHaE",
+    authDomain: "whatsapp-mern-ab82f.firebaseapp.com",
+    projectId: "whatsapp-mern-ab82f",
+    storageBucket: "whatsapp-mern-ab82f.appspot.com",
+    messagingSenderId: "539604624098",
+    appId: "1:539604624098:web:48712096b0f2df418b74bb"
+  };
