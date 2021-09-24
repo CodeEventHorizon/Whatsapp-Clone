8226a294f6f112eb74066917e9fcfe634d0ccb30
Nikoloz Muladze <nikoloz.9374@gmail.com>
2021-09-24 21:38:09 +0400

    Styled a sidebar

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import './Sidebar.css'
+import SidebarChat from './SidebarChat'
 import DonutLargeIcon from '@material-ui/icons/DonutLarge'
 import ChatIcon from '@material-ui/icons/Chat'
 import MoreVertIcon from '@material-ui/icons/MoreVert'
 import { Avatar, IconButton } from '@material-ui/core'
+import { SearchOutlined } from '@material-ui/icons'
 
 function Sidebar() {
     return (
@@ -22,6 +24,19 @@ function Sidebar() {
                         <MoreVertIcon />
                     </IconButton>
                 </div>
+            </div>
+
+            <div className="sidebar__search">
+                <div className="sidebar__searchContainer">
+                    <SearchOutlined />
+                    <input placeholder="Search or start new chat" type="text" />
+                </div>
+            </div>
+
+            <div className="sidebar__chats">
+                <SidebarChat />
+                <SidebarChat />
+                <SidebarChat />
             </div>
         </div>
     )
