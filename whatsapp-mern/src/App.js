41087b176dfde4125013a5316b7c513ddc79d729
Nikoloz Muladze <nikoloz.9374@gmail.com>
2021-09-26 19:28:24 +0400

    added pusher to the frontend, making it realtime

--- whatsapp-mern/src/App.js
+++ whatsapp-mern/src/App.js
@@ -1,8 +1,23 @@
+import React, { useEffect } from 'react';
 import './App.css';
 import Chat from './Chat';
 import Sidebar from './Sidebar';
+import Pusher from 'pusher-js';
 
 function App() {
+
+  useEffect(() => {
+    // once
+    const pusher = new Pusher('42f3705babead6ac3701', {
+      cluster: 'eu'
+    });
+
+    const channel = pusher.subscribe('messages');
+    channel.bind('inserted', (data) => {
+      alert(JSON.stringify(data));
+    });
+  }, [])
+
   return (
     <div className="app">
       <div className="app_body">
